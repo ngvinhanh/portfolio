eb47bf426776a6526796471520dd528c5b4c37ce
ngvinhanh <ngvinhanh@gmail.com>
2021-12-02 12:24:15 +0100

    fix app

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,7 +11,7 @@ function Contact(){
         <div className='contactContainer row grid wide font-loader' id='contactId'>
             <p className='l-12 m-12 c-12 message' >If you'd like to work together or have any questions then please get in touch - I'd love to hear from you !
             </p>
-            <div>
+            <div className='row grid wide font-loader'>
                 <h1 className='l-4 m-4 c-12'>Contact</h1>
                 <div  className='contact l-8 m-8 c-12'>
                     <p><span><FontAwesomeIcon icon={faEnvelope} /></span><strong>Email:</strong> ngvinhanh@gmail.com</p>
