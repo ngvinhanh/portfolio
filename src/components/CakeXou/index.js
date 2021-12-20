b73b8ec9e1cb9f8c650de6fb9152bba518eb6783
ngvinhanh <ngvinhanh@gmail.com>
2021-12-20 11:20:40 +0100

    text

--- src/components/CakeXou/index.js
+++ src/components/CakeXou/index.js
@@ -13,8 +13,8 @@ function CakeXou(){
                 <img src= {logo} alt='logo' className='logo l-4 m-5 c-12'/>
                 <div className='cakeXouPresentation font-loader l-8 m-7 c-12'>
                     <h1>My Story</h1>
-                    <p>I graduated with a degree in Finance and Economics, but design and creativity have always been something I want to do.   </p>
-                    <p>Feeling inspired after making a homemade Christmas cake, I poured my creativity energy into building a local baking business called CakeXou. The five years with CakeXou was a special experience, in which I had the chance to design and make pretty figures that are edible!</p>
+                    <p>I graduated with a degree in Finance and Economics, but design and creativity have always been something I want to do. </p>
+                    <p>Feeling inspired after making a homemade Christmas cake, I poured my creativity energy into building a local baking business called CakeXou. The five years with CakeXou was a special experience, in which I had the chance to design and make pretty figures that are edible !</p>
                     <p>Here are some of my cakes
                     <span><FontAwesomeIcon icon={faHeart} /></span>
                     </p>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,8 +9,7 @@ import './Contact.css'
 function Contact(){
     return (
         <div className='contactContainer row grid wide font-loader' id='contactId'>
-            <p className='l-12 m-12 c-12 message' >If you'd like to work together or have any questions then please get in touch - I'd love to hear from you !
-            </p>
+            <p className='l-12 m-12 c-12 message' >If you want to collaborate or have any questions, please get in touch - I'd love to hear from you !</p>
             <div className='row grid wide font-loader'>
                 <h1 className='l-4 m-4 c-12'>Contact</h1>
                 <div  className='contact l-8 m-8 c-12'>
